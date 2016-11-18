b04a84a85bdc64c76466a23cc6f056a1076124c1
Maglethong Spirr <maglethongspirr@gmail.com>
2016-11-18 08:20:49 -0200

    Uploading Version from Dragon Server

---
+++ Assets/CamControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamControl : MonoBehaviour {
+
+	public Vector3 Mov1; 
+	public Vector3 Mov2; 
+	public Vector3 Mov3; 
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate(Mov1, Space.Self);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Rotate (Mov2, Space.Self);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate (Mov3, Space.Self);
+		}
+	}
+}
