9e4ed60212bb6af0a7b9d028513c0a72f84e1bb2
Marcos Defina <marcosdefina@gmail.com>
2018-12-17 18:05:20 -0200

    fixing low profile LoadingScene

--- Assets/Programação/Scripts/Cutscenes/LoadingScreen.cs
+++ Assets/Programação/Scripts/Cutscenes/LoadingScreen.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9129f4275feeb4daccf6cb3998e2d050eb2ae590dc6b9b92d6e3066f503cfeb
-size 2465
+oid sha256:9e9792168f8c4aa9ab433f53e56041ad016a2ca62b30dcac250e3103d0e51f63
+size 2449
