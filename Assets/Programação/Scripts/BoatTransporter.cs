19474c9a3c2a3fca4418e184e8670d196ee161e1
Marcos Defina <marcosdefina@gmail.com>
2018-12-18 15:59:25 -0200

    deleting HighQuality Textures

--- Assets/Programação/Scripts/BoatTransporter.cs
+++ Assets/Programação/Scripts/BoatTransporter.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6499e4e71048fdb7e3106707e40dc2666c96c646d9f2ee433f3d0227a04c289
-size 2025
+oid sha256:5470b20ee35ef65a389882baa6e1f92262dcab67b82a7925d6d0d36cb71234ba
+size 2220
