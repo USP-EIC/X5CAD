93f9bdf348647d14374226d99d34d3ab80ee1d77
Marcos Defina <marcosdefina@gmail.com>
2019-03-26 23:12:55 -0300

    Lucas's modifications

---
+++ Assets/Programação/PrimeiraCena/IntroFirstScene.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:23eb1411fae10e63868def62b4fd0e20baa27c505d612344dde5c522fa5e6a9c
+size 837
---
+++ Assets/_Quests/Scripts/PressEBalsa.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e25aa7a7783c69b108545da839c0fff9b4c526d6597b7996f8573f0046efa37f
+size 656
